4b610247d9945d5218053e6902fac211e7e320e9
dsupchuk <d.supchuk@gmail.com>
2022-05-01 18:19:24 +0300

    first and last commit

---
+++ src/components/footer/footer.js
@@ -0,0 +1,10 @@
+import "./footer.css";
+export function Footer() {
+  return (
+    <div className="footer-list">
+      <p>Email: asav@nvl.com</p>
+      <p>Phone: +1234567890</p>
+      <p>Insta: fjvlqx</p>
+    </div>
+  );
+}
---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import "./header.css";
+export function Header() {
+  return (
+    <>
+      <ul className="header-list">
+        <li>Home</li>
+        <li>Contact</li>
+        <li>Help</li>
+        <li>Map</li>
+        <li>LogIn</li>
+      </ul>
+    </>
+  );
+}
